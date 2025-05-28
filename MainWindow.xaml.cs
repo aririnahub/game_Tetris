b43aae8feca2df57ccc1ff9b6f774d014a870b1c
aririnahub <asvlasova2005@gmail.com>
2025-05-28 12:56:20 +0300

    Game Tetris

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace TetrisGame
+{
+    public partial class MainWindow : Window
+    {
+        private TetrisGame tetrisGame;
+        private DispatcherTimer gameTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            tetrisGame = new TetrisGame(GameGrid);
+            gameTimer = new DispatcherTimer();
+            gameTimer.Tick += GameLoop;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(500);
+            gameTimer.Start();
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+            tetrisGame.MoveDown();
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                tetrisGame.MoveLeft();
+            }
+            else if (e.Key == Key.Right)
+            {
+                tetrisGame.MoveRight();
+            }
+            else if (e.Key == Key.Down)
+            {
+                tetrisGame.MoveDown();
+            }
+            else if (e.Key == Key.Space)
+            {
+                tetrisGame.Rotate();
+            }
+        }
+    }
+}
---
+++ TetrisGame.cs
@@ -0,0 +1,125 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace TetrisGame
+{
+    public class TetrisGame
+    {
+        public List<Rectangle> TetrisBlocks { get; private set; }
+        private Grid gameGrid;
+
+        public TetrisGame(Grid grid)
+        {
+            TetrisBlocks = new List<Rectangle>();
+            gameGrid = grid;
+            // инициализация игровых элементов
+            // начальное создание фигур
+            CreateTetromino();
+        }
+
+        private void CreateTetromino()
+        {
+            // создание случайной фигуры, например, квадрата
+            Rectangle block = new Rectangle
+            {
+                Width = 20,
+                Height = 20,
+                Fill = Brushes.Blue
+            };
+            TetrisBlocks.Add(block); // добавляем фигуру в коллекцию
+            gameGrid.Children.Add(block); // добавляем фигуру на игровое поле
+            Canvas.SetTop(block, 0); // устанавливаем начальное положение
+            Canvas.SetLeft(block, 140);
+        }
+
+        public void MoveLeft()
+        {
+            foreach (Rectangle block in TetrisBlocks)
+            {
+                double left = Canvas.GetLeft(block);
+                if (left > 0)
+                {
+                    Canvas.SetLeft(block, left - 20); // сдвигаем фигуру влево на 20 пикселей
+                }
+            }
+        }
+
+        public void MoveRight()
+        {
+            foreach (Rectangle block in TetrisBlocks)
+            {
+                double left = Canvas.GetLeft(block);
+                if (left < 280) // ширина игрового поля - ширина фигуры
+                {
+                    Canvas.SetLeft(block, left + 20); // сдвигаем фигуру вправо на 20 пикселей
+                }
+            }
+        }
+
+        public void MoveDown()
+        {
+            foreach (Rectangle block in TetrisBlocks)
+            {
+                double top = Canvas.GetTop(block);
+                if (top < 480) // высота игрового поля - высота фигуры
+                {
+                    Canvas.SetTop(block, top + 20); // сдвигаем фигуру вниз на 20 пикселей
+                }
+                else
+                {
+                    CheckLines(); // проверяем заполненные линии
+                    CreateTetromino(); // создаем новую фигуру
+                }
+            }
+        }
+
+        public void Rotate()
+        {
+            // поворот фигуры
+        }
+
+        private void CheckLines()
+        {
+            for (int i = 480; i > 0; i -= 20)
+            {
+                bool lineFilled = true;
+                foreach (Rectangle block in TetrisBlocks)
+                {
+                    if (Canvas.GetTop(block) == i)
+                    {
+                        lineFilled = false;
+                        break;
+                    }
+                }
+
+                if (lineFilled)
+                {
+                    RemoveLine(i);
+                    i += 20;
+                }
+            }
+        }
+
+        private void RemoveLine(double line)
+        {
+            List<Rectangle> blocksToRemove = TetrisBlocks.FindAll(block => Canvas.GetTop(block) == line);
+            foreach (Rectangle block in blocksToRemove)
+            {
+                TetrisBlocks.Remove(block);
+                gameGrid.Children.Remove(block);
+            }
+
+            foreach (Rectangle block in TetrisBlocks)
+            {
+                double top = Canvas.GetTop(block);
+                if (top < line)
+                {
+                    Canvas.SetTop(block, top + 20);
+                }
+            }
+        }
+    }
+}
